d660fdecfcbf5ceed7fcb81d7d9b66c821ebb330
RafaelUeda <rafafe2003@gmail.com>
2025-01-23 10:31:01 -0300

    initializing project

--- library.ts
+++ library.ts
@@ -1,30 +1,35 @@
 // O sistema deve permitir adicionar livros, registrar usuários, emprestar livros e listar os livros emprestados por cada usuário.
 // O sistema não precisa ter uma interface visual, faça toda demonstração de dados pelo console.log.
 
-export class Book {
+
+// Classe responsável por representar o livro
+export class Book { 
     private name: string;
     private author: string;
 }
 
+// Classe responsável por representar o usuário que usa a biblioteca
 export class User {
 
 }
 
+// Classe do estudante que herda (inheritance) User 
 export class StudentUser extends User {
 
 }
 
-export class ProfessorUser extends User {
 
+// Classe do professor que herda (inheritance) User
+export class ProfessorUser extends User {
+    
 }
 
+// Classe responsável por representar a biblioteca
 export class Library {
 
 }
 
+// Classe principal, responsável por executar o fluxo de sistema, instanciar as classes e executar os métodos
 export class System {
-
+    
 }
-
-
-console.log('Hello')
