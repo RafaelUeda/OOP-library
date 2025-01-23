82b1a0bdc227f3d2615be33238afdd4846b70644
RafaelUeda <rafafe2003@gmail.com>
2025-01-23 15:40:52 -0300

    OOP concepts

--- library.ts
+++ library.ts
@@ -1,6 +1,6 @@
 // O sistema deve permitir adicionar livros, registrar usuários, emprestar livros e listar os livros emprestados por cada usuário.
 // O sistema não precisa ter uma interface visual, faça toda demonstração de dados pelo console.log.
-
+const prompt = require('prompt-sync')();
 
 // Classe responsável por representar o livro
 export class Book {
@@ -11,20 +11,29 @@ export class Book {
         this.bookName = bookName;
         this.author = author;
     }
+
+    public getBookInfo(): string {
+        return this.bookName, this.author;
+    }
 }
 
-// Classe responsável por representar o usuário que usa a biblioteca
 export class User {
     private userName: string;
 
-    // constructor is a special method that is called when a new instance of the "User" class
-    // and it must be a string
     constructor(userName: string) {
         this.userName = userName;
     }
 
     public getUserName(): string {
         return this.userName
+    }
+
+    public getBooks(): [Book] {
+        return books[]
+    }
+
+    public lendBook(): Book {
+        
     }
 }
 
@@ -39,7 +48,38 @@ export class ProfessorUser extends User {
 
 }
 
-// Classe responsável por representar a biblioteca
-export class Library {
+// Classe responsável por representar a biblioteca, contém livros e usuários
+export class Library extends Book {
 
 }
+
+// Classe principal, responsável por executar o fluxo de sistema, instanciar as classes e executar os métodos
+export class System extends User, Book, Library {
+    // you should define class properties and initialize them inside the constructor
+    private user!: User;
+    private book!: Book
+ 
+    constructor() {
+        let option: number;
+        do {
+            option = parseInt(prompt("1 - Registrar um livro \n 2 - Registrar um usuário \n 9 - Sair da biblioteca \n") || 0);
+            if (option === 1) {
+                let bookName: string = prompt("Digite o nome do livro: ")
+                let bookAuthor: string = prompt("Digite o nome do autor do livro: ")
+                this.book = new Book(bookName, bookAuthor)
+                console.log(this.book.getBookInfo());
+            } else if (option === 2) {
+                let userName: string = prompt("Digite o nome do usuário: ")
+                this.user = new User(userName);
+                console.log(this.user.getUserName());
+            } else if (option === 9) {
+                console.log("Saindo da biblioteca...");
+            } else {
+                console.log("Opção inválida")
+            }
+        }
+        while (option !== 9)
+    }
+}
+
+new System();
---
+++ studying.ts
@@ -0,0 +1,47 @@
+// definindo uma classe Example
+class Example {
+    // propriedades e métodos vem dentro da classe
+}
+
+class Car {
+    private name: string;
+    private model: string;
+    private year: number;
+
+    constructor(name: string, model: string, year: number) {
+        this.name = name;
+        this.model = model;
+        this.year = year;
+    } // este é o construtor da classe Car
+    // o construtor é uma classe especial que é chamado quando um objeto (instância de uma classe) é criado.
+    // ele é utilizado para iniciar um objeto com valores padrões;
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public setName(name: string): void {
+        this.name = name;
+    }
+
+    public getModel(): string {
+        return this.model;
+    }
+
+    public setModel(model: string): void {
+        this.model = model;
+    }
+
+    public getYear(): number {
+        return this.year;
+    }
+
+    public setYear(year: number): void {
+        this.year = year;
+    }
+}
+
+// um objeto é uma instância de uma classe
+
+let creta = new Car("Creta", "Renault", 2016)
+console.log(creta.getName(), creta.getModel(), creta.getYear())
