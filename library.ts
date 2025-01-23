4c758405820dd6af56dae148755d33bccd390bdd
RafaelUeda <rafafe2003@gmail.com>
2025-01-23 10:06:39 -0300

    initializing project

--- library.ts
+++ library.ts
@@ -1,1 +1,30 @@
+// O sistema deve permitir adicionar livros, registrar usuários, emprestar livros e listar os livros emprestados por cada usuário.
+// O sistema não precisa ter uma interface visual, faça toda demonstração de dados pelo console.log.
+
+export class Book {
+    private name: string;
+    private author: string;
+}
+
+export class User {
+
+}
+
+export class StudentUser extends User {
+
+}
+
+export class ProfessorUser extends User {
+
+}
+
+export class Library {
+
+}
+
+export class System {
+
+}
+
+
 console.log('Hello')
